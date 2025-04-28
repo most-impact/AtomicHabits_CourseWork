f2ecec929de0e9b2d2eb155113107ed3323b7e7f
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-04-28 12:14:33 +0300

    add pagination.py

--- habit/views.py
+++ habit/views.py
@@ -1,13 +1,14 @@
 from rest_framework import generics, permissions
-from rest_framework.pagination import PageNumberPagination
+
 from .models import Habit
+from .pagination import CustomPagination
 from .serializers import HabitSerializers
 
 
 class HabitListCreateView(generics.ListCreateAPIView):
     serializer_class = HabitSerializers
     permission_classes = [permissions.IsAuthenticated]
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         return Habit.objects.filter(user=self.request.user).order_by('id')
