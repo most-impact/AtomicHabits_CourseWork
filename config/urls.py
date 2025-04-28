78506329c16ae590ae76e57b3c0a01e2271c1ced
Dmitriy Kurilko <dimakurilko0606@mail.ru>
2025-04-28 12:11:28 +0300

    new commit

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,12 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from users.views import UserRegistrationView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -29,4 +35,7 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
     path("api/", include("habit.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', UserRegistrationView.as_view(), name='register'),
 ]
--- habit/views.py
+++ habit/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, permissions
-
+from rest_framework.pagination import PageNumberPagination
 from .models import Habit
 from .serializers import HabitSerializers
 
@@ -7,12 +7,10 @@ from .serializers import HabitSerializers
 class HabitListCreateView(generics.ListCreateAPIView):
     serializer_class = HabitSerializers
     permission_classes = [permissions.IsAuthenticated]
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
-        return Habit.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
+        return Habit.objects.filter(user=self.request.user).order_by('id')
 
 
 class HabitRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
